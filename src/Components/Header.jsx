8df7f582c7b2ec0c61904d16ec763de0baaf2435
Valery Sakalou <mimino730@tut.by>
2021-03-22 23:39:39 +0300

    lesson 13

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img className='logo' src='https://cdn1.truelancer.com/upload-full/984522-6a26c28a68ac5cb7091da4a72a4416785f0abd05.jpg' />
+    </header>
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <img src='https://st2.depositphotos.com/1030327/12301/i/950/depositphotos_123015928-stock-photo-scenic-view-of-lake-with.jpg' />
+        <div>
+            avatar+descriptoin
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            New post
+        </div>
+        <div>
+            post1
+         </div>
+        <div>
+            post2
+        </div>
+    </div>
+}
+export default Profile;
