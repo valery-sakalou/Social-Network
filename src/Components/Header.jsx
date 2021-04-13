1ef9db106e3767209be7f8d01b265c2ada4c8e92
Valery Sakalou <mimino730@tut.by>
2021-04-14 00:02:20 +0300

    Lesson14

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img className='logo' src='https://cdn1.truelancer.com/upload-full/984522-6a26c28a68ac5cb7091da4a72a4416785f0abd05.jpg' />
     </header>
 }
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <img src='https://st2.depositphotos.com/1030327/12301/i/950/depositphotos_123015928-stock-photo-scenic-view-of-lake-with.jpg' />
         <div>
             avatar+descriptoin
@@ -11,12 +12,14 @@ const Profile = () => {
         </div>
         <div>
             New post
-        </div>
-        <div>
+        </div >
+        <div className ={s.posts}>
+        <div className ={s.item}>
             post1
          </div>
-        <div>
+        <div className ={s.item}>
             post2
+        </div>
         </div>
     </div>
 }
